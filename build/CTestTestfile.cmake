d6fed9fed3a374a50ad2c99d6f428895e9f134b8
Unknown <carlstand@gmail.com>
2017-12-08 23:44:57 +0800

    Auto stash before merge of "master" and "origin/master"

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carlstand/ROS_iiwa_Demo/src
-# Build directory: /home/carlstand/ROS_iiwa_Demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
