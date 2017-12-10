84883e77cb30be4edb262f5253313d921f590def
Unknown <carlstand@gmail.com>
2017-12-10 10:01:21 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ src/opencv/scripts/ImageReader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Simple talker demo that published std_msgs/Strings messages
+# to the 'chatter' topic
+
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+# from std_msgs.msg import String
+
+
+def ImageReader():
+    pub = rospy.Publisher('image_topic', Image, queue_size=10)
+    rospy.init_node('ImageReader', anonymous=False)
+    rate = rospy.Rate(0.5)
+    img = cv2.imread("/home/carlstand/ROS_iiwa_Demo/src/opencv/scripts/example.png")
+    bridge = CvBridge()
+    while not rospy.is_shutdown():
+        # hello_str = "hello world %s" % rospy.get_time()
+        # rospy.loginfo(hello_str)
+        pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        ImageReader()
+    except rospy.ROSInterruptException:
+        cv2.destroyAllWindows()
+        pass
